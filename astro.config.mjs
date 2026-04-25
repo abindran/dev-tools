ff753c84e2fe346e1f245badc84c74d8ff60eeb7
abindran <abindran.r@zohocorp.com>
2026-04-25 13:31:23 +0530

    SEO optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,30 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://base64decode.online',
   output: 'server',
   adapter: cloudflare(),
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          es: 'es',
+          fr: 'fr',
+        },
+      },
+    }),
+  ],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es', 'fr'],
     routing: {
-      prefixDefaultLocale: false
-    }
-  }
+      prefixDefaultLocale: false,
+    },
+  },
 });
